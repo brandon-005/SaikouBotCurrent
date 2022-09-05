2110fabe23e2a41d1c33349001b659320ce0d3a5
Brandon <brandong9@sky.com>
2022-09-05 16:11:58 +0100

    feat(emit): Emit Developer Command
    A command used for emitting certain Discord events for testing.

---
+++ src/commands/dev-only/emit.ts
@@ -0,0 +1,42 @@
+import { Command, ApplicationCommandOptionType } from 'discord.js';
+
+const command: Command = {
+	config: {
+		commandName: 'emit',
+		commandAliases: ['botban'],
+		commandDescription: 'DEVELOPER ONLY - Emitting events.',
+		limitedChannel: 'None',
+		developerOnly: true,
+		slashOptions: [
+			{
+				name: 'event',
+				description: 'Which event you would like to emit.',
+				type: ApplicationCommandOptionType.String,
+				required: true,
+				choices: [
+					{
+						name: 'guildMemberAdd',
+						value: 'guildMemberAdd',
+					},
+					{
+						name: 'guildMemberRemove',
+						value: 'guildMemberRemove',
+					},
+				],
+			},
+			{
+				name: 'user',
+				description: 'User to emit event with.',
+				type: ApplicationCommandOptionType.User,
+				required: true,
+			},
+		],
+	},
+	run: async ({ bot, interaction, args }) => {
+		bot.emit(args[0], interaction.options.getMember('user'));
+
+		return interaction.followUp({ content: `Successfully emitted the **${args[0]}** event.` });
+	},
+};
+
+export = command;
